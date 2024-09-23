d0b90880ddd69bd9d56258f2181cca6f49eb1117
Joao Paulo <joaopaulolacerda911@gmail.com>
2024-09-23 19:51:31 -0300

    project is done

--- src/app/(drawer)/_layout.tsx
+++ src/app/(drawer)/_layout.tsx
@@ -24,13 +24,232 @@ export default function DrawerLayout() {
           } as CustomOptions
         }
       />
-      <Drawer.Screen
+      {/* <Drawer.Screen
         name="config"
         options={
           {
-            title: "Entrada",
-            iconName: "inbox",
+            title: "Com estrela",
+            iconName: "star-outline",
             notifications: 3,
+            isDivider: true,
+          } as CustomOptions
+        }
+      /> */}
+      {/* <Drawer.Screen
+        name="postned"
+        options={
+          {
+            title: "Com estrela",
+            iconName: "schedule",
+            notifications: 3,
+            isDivider: true,
+          } as CustomOptions
+        }
+      />
+      <Drawer.Screen
+        name="important"
+        options={
+          {
+            title: "Importante",
+            iconName: "label-important-outline",
+          } as CustomOptions
+        }
+      />
+      <Drawer.Screen
+        name="send"
+        options={
+          {
+            title: "Enviado",
+            iconName: "send",
+          } as CustomOptions
+        }
+      /> */}
+      {/* <Drawer.Screen
+        name="programed"
+        options={
+          {
+            title: "Programado",
+            iconName: "schedule-send",
+          } as CustomOptions
+        }
+      /> */}
+      {/* <Drawer.Screen
+        name="exit-box"
+        options={
+          {
+            title: "Caixa de saída",
+            iconName: "outbox",
+          } as CustomOptions
+        }
+      /> */}
+      {/* <Drawer.Screen
+        name="sketch"
+        options={
+          {
+            title: "Rascunho",
+            iconName: "note",
+          } as CustomOptions
+        }
+      /> */}
+
+      {/* <Drawer.Screen
+        name="all-email"
+        options={
+          {
+            title: "Todos os e-mails",
+            iconName: "email",
+          } as CustomOptions
+        }
+      /> */}
+
+      {/* <Drawer.Screen
+        name="span"
+        options={
+          {
+            title: "Spam",
+            iconName: "info-outline",
+          } as CustomOptions
+        }
+      /> */}
+
+      {/* <Drawer.Screen
+        name="trash"
+        options={
+          {
+            title: "Lixeira",
+            iconName: "delete-outline",
+            isDivider: true,
+          } as CustomOptions
+        }
+      /> */}
+      {/* <Drawer.Screen
+        name="read"
+        options={
+          {
+            title: "Lixeira",
+            iconName: "label-important-outline",
+            sectionTitle: "Marcadores",
+          } as CustomOptions
+        }
+      /> */}
+      {/* <Drawer.Screen
+        name="help"
+        options={
+          {
+            title: "Lixeira",
+            iconName: "label-important-outline",
+            isDivider: true,
+          } as CustomOptions
+        }
+      /> */}
+      {/* <Drawer.Screen
+        name="general"
+        options={
+          {
+            title: "Criar novo",
+            iconName: "add",
+            isDivider: true,
+          } as CustomOptions
+        }
+      /> */}
+      <Drawer.Screen
+        name="stars"
+        options={
+          {
+            title: "Com estrela",
+            iconName: "star-outline",
+            isDivider: true,
+          } as CustomOptions
+        }
+      />
+      <Drawer.Screen
+        name="important"
+        options={
+          {
+            title: "Importante",
+            iconName: "label-important-outline",
+            isDivider: true,
+          } as CustomOptions
+        }
+      />
+      <Drawer.Screen
+        name="send"
+        options={
+          {
+            title: "Enviados",
+            iconName: "send",
+            isDivider: true,
+          } as CustomOptions
+        }
+      />
+      <Drawer.Screen
+        name="programed"
+        options={
+          {
+            title: "Enviados",
+            iconName: "schedule-send",
+            isDivider: true,
+          } as CustomOptions
+        }
+      />
+      <Drawer.Screen
+        name="exit-box"
+        options={
+          {
+            title: "Caixa de saída",
+            iconName: "outbox",
+            isDivider: true,
+          } as CustomOptions
+        }
+      />
+      <Drawer.Screen
+        name="sketch"
+        options={
+          {
+            title: "Rascunho",
+            iconName: "note",
+            isDivider: true,
+          } as CustomOptions
+        }
+      />
+
+      <Drawer.Screen
+        name="all-email"
+        options={
+          {
+            title: "Todos emails",
+            iconName: "email",
+            isDivider: true,
+          } as CustomOptions
+        }
+      />
+      <Drawer.Screen
+        name="span"
+        options={
+          {
+            title: "Span",
+            iconName: "info-outline",
+            isDivider: true,
+          } as CustomOptions
+        }
+      />
+      <Drawer.Screen
+        name="trash"
+        options={
+          {
+            title: "Lixeira",
+            iconName: "delete-outline",
+            isDivider: true,
+            sectionTitle: "Marcadores",
+          } as CustomOptions
+        }
+      />
+      <Drawer.Screen
+        name="new-marker"
+        options={
+          {
+            title: "Criar novo",
+            iconName: "add",
             isDivider: true,
           } as CustomOptions
         }
--- src/components/MenuButtom/index.tsx
+++ src/components/MenuButtom/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { Pressable } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
+import { useNavigation, DrawerActions } from "@react-navigation/native";
 
 export const MenuButton: React.FC = () => {
+  const navigate = useNavigation();
+
   return (
-    <Pressable>
+    <Pressable onPress={() => navigate.dispatch(DrawerActions.openDrawer())}>
       <MaterialIcons name="menu" size={22} color={colors.white} />
     </Pressable>
   );
